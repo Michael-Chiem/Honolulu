96dc68a9e70dc7a1f142c34f45ce43e3c6d3f813
Michael Chiem <michaelchiem1@hotmail.com>
2024-05-13 16:05:16 -0600

    Adding Pages JSX file

---
+++ pages/ProjectPage.jsx
@@ -0,0 +1,38 @@
+// This is a static page mocking an "About Us" section for our fake user data
+export default function AboutPage() {
+    return (
+      <div className="container pt-4">
+        <p>
+          Since embarking on my journey into the realm of Full Stack Development approximately six months
+          ago, I've been on a mission to bolster my existing networking background with a robust skill set in
+          coding and web development. The decision to enroll in a cohort dedicated to this pursuit has proven to 
+          be one of the most enriching experiences of my professional life thus far.
+
+          Throughout this intensive boot camp, I've been thoroughly engaged in a multitude of intriguing
+          concepts and practical applications, constantly testing and pushing the boundaries of my mental 
+          faculties. It's been a challenging yet immensely rewarding journey, one that has not only honed my
+          technical abilities but also instilled in me a newfound sense of resilience and determination.
+        </p>
+
+        <p>
+          In an effort to showcase the progress I've made over the past six months, I've taken the initiative to
+          develop a personal website. Leveraging technologies such as React JS alongside a host of other
+          dependencies, I've meticulously crafted a demo site that serves as a testament to my growth and
+          proficiency in Full Stack Development.
+
+        </p>
+        
+        <p>
+          It's important for me to emphasize that my primary goal with this endeavor is not financial gain, but 
+          rather the opportunity to share my passion for web development and offer my skills to those in need. If 
+          you're in search of a dynamic and professionally crafted website for your business or project, I invite you
+          to reach out to me. I'm wholeheartedly committed to bringing your vision to life and delivering a product 
+          that exceeds your expectations.
+
+          Please feel free to contact me via email at michaelchiem@hotmail.com to discuss your project
+          requirements in further detail. I'm excited about the prospect of collaborating with you and helping you
+          achieve your online goals.
+        </p>
+      </div>
+    );
+  }
